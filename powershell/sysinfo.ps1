93a1e9511caf024221af302991da171145f4715c
juhasjd <122639934+juhasjd@users.noreply.github.com>
2023-02-05 18:23:26 -0500

    Update sysinfo.ps1

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -7,6 +7,6 @@ $PS = $HOST.Version.Major
 
 
 Write-Host("This is this machine's IP: $IP")
-$BODY = ("This machine's IP is $IP . The user is $USER . Hostname is $HOSTNAME . Powershell version is $ps .")
+$BODY = ("This machine's IP is $IP . The user is $USER . Hostname is $HOSTNAME . Powershell version is $PS .")
 
 Send-MailMessage -To "leonardf@ucmail.edu" -From "juhasjon9@gmail.com" -Subject "IT3038c Windows SysInfo" -Body $BODY -SmtpServer smpt.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
