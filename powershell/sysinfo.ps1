fb8c075c856ebcc55da197b6b71db112ab933417
juhasjd <122639934+juhasjd@users.noreply.github.com>
2023-02-05 18:44:27 -0500

    Update sysinfo.ps1

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,5 +1,5 @@
 ﻿function getIP {
-(get-netipaddress).ipv4adress |Select-String "192.*"
+(get-netipaddress).ipv4address |Select-String "192*"
 }
 $IP = getIP
 $USER = whoami
@@ -9,4 +9,4 @@ $PS = $HOST.Version.Major
 Write-Host("This is this machine's IP: $IP")
 $BODY = ("This machine's IP is $IP . The user is $USER . Hostname is $HOSTNAME . Powershell version is $PS .")
 
-Send-MailMessage -To "leonardf@ucmail.edu" -From "juhasjon9@gmail.com" -Subject "IT3038c Windows SysInfo" -Body $BODY -SmtpServer smpt.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "juhasjon9@gmail.com" -Subject "IT3038c Windows SysInfo" -Body $BODY -SmtpServer smpt.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
