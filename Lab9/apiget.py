ea2a7ea3716fb973b75c8526217e3387670cff60
juhasjd <122639934+juhasjd@users.noreply.github.com>
2023-04-02 17:24:27 -0400

    Update apiget.py

--- Lab9/apiget.py
+++ Lab9/apiget.py
@@ -3,5 +3,4 @@ import json
 response = requests.get('http://localhost:3000/color')
 data = response.json()
 
-for widget in data:
-    print(f"{widget['name']} is {widget['color']}.")
+print(f"{widget['name']} is {widget['color']}.")
