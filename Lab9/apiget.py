3e695bcc2a14f404bc58c3a4e0f3ddb64262307e
juhasjd <122639934+juhasjd@users.noreply.github.com>
2023-04-02 17:21:21 -0400

    Add files via upload

---
+++ Lab9/apiget.py
@@ -0,0 +1,7 @@
+import requests
+import json
+response = requests.get('http://localhost:3000/color')
+data = response.json()
+
+for widget in data:
+    print(f"{widget['name']} is {widget['color']}.")
